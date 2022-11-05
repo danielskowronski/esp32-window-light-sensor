d7d8686d0aee7a85c542309e81914bde52cbf394
Daniel Skowroński <daniel@skowron.ski>
2022-11-05 10:19:20 +0100

    error handling, hardware spec update

--- software/esp32-window-light-sensor/TemperatureSensor.h
+++ software/esp32-window-light-sensor/TemperatureSensor.h
@@ -1,5 +1,6 @@
+#include <math.h>
 #include <Wire.h>
-//#include <Adafruit_Sensor.h>
+#include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 
 Adafruit_BMP280 HW611;
@@ -8,22 +9,34 @@ struct TemperatureSensor : Service::TemperatureSensor {
   SpanCharacteristic *temperature;
   
   TemperatureSensor() : Service::TemperatureSensor(){
-    temperature=new Characteristic::CurrentTemperature(0);
+    temperature = new Characteristic::CurrentTemperature(0);
     temperature->setRange(-40,85);
         
-    Serial.print("Configuring Temperature");
-    Serial.print("\n");
+    Serial.print("Configuring Temperature: ");
 
-    HW611.begin(BMP280_I2C_ADDRESS); // TODO: handle initialization errors
+    Wire.begin();
+    bool initialized = HW611.begin(BMP280_I2C_ADDRESS, BMP280_CHIPID);
+
+    if (initialized) {
+      Serial.println("OK");
+    }
+    else {
+      Serial.println("FAILED");
+    }
   } 
 
   void loop(){
     if(temperature->timeVal()>2500){
-      float celsius=HW611.readTemperature();;
+      float celsius=HW611.readTemperature();
+      if (isnan(celsius)){
+        celsius=-40; // Characteristic::StatusFault does not work so applying workaround
+      }
       temperature->setVal(celsius); 
+
       LOG1("Temperature Update: ");
       LOG1(celsius);
-      LOG1(" °C\n");
+      LOG1(" °C");
+      LOG1("\n");
     }
   } 
 };
